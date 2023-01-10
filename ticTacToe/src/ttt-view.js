56aa6f9620eb87bf8c0d502fbd1c757fa788dbdd
lorenzo <lorenzotijerina@gmail.com>
2023-01-10 17:25:29 -0500

    complete board set up

--- ticTacToe/src/ttt-view.js
+++ ticTacToe/src/ttt-view.js
@@ -1,14 +1,23 @@
 class View {
   constructor(game, el) {}
 
-  setupBoard() {}
-  
+  setupBoard() {
+    const ul = document.createElement("ul");
+    for (let rowIdx = 0; rowIdx < 3; rowIdx++) {
+      for (let colIdx = 0; colIdx < 3; colIdx++) {
+        const li = document.createElement("li");
+        li.dataset.pos = JSON.stringify([rowIdx, colIdx]);
+        ul.append(li);
+      }
+    }
+    this.el.append(ul);
+  }
+
   bindEvents() {}
 
   handleClick(e) {}
 
   makeMove(square) {}
-
 }
 
 module.exports = View;
